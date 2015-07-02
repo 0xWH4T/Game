f437c7251751f2e74718abe2da10b60b5973c35e
Justin Savino <justin.savino2@gmail.com>
2015-07-02 18:09:01 -0400

    messing with opengl

--- src/draw.h
+++ src/draw.h
@@ -2,6 +2,30 @@
 #define DRAW_71279
 #include <GLFW\glfw3.h>
 
-void draw() {}
+const double DEG2RAD = 3.14159/180;
+/*void drawCircle(double radius) 
+{
+	glBegin(GL_LINE_LOOP);
+	for(int i = 0; i &amp; lt; 360; i++)
+	{
+
+	}
+}*/
+class Quad
+{
+public:
+	Quad()
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glColor3f(0.0, 1.0, 2.0);
+		glBegin(GL_POLYGON);
+		glVertex3f(2.0, 4.0, 0.0);
+		glVertex3f(8.0, 4.0, 0.0);
+		glVertex3f(8.0, 6.0, 0.0);
+		glVertex3f(2.0, 6.0, 0.0);
+		glEnd();
+		glFlush();
+	}
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,49 @@
 #include <GLFW\glfw3.h>
+#include "draw.h"
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+static void error_callback(int error, const char* description)
+{
+	fputs(description, stderr);
+}
 
 int main(int argc, char** argv)
 {
-  //changes
-  return 0;
+	//initialize
+	glfwSetErrorCallback(error_callback);
+	if(!glfwInit())
+		exit(GL_FALSE);
+
+	//create window
+	int frameWidth = 800;int frameHeight = 600;
+	GLFWwindow* window = glfwCreateWindow(frameWidth, frameHeight, "Window", NULL, NULL);
+	if(!window)
+	{
+		glfwTerminate();
+		exit(GL_FALSE);
+	}
+
+	glfwMakeContextCurrent(window); //make opengl context current
+	glfwSwapInterval(1);
+
+	while(!glfwWindowShouldClose(window))
+	{
+
+		glClearColor(0.0, 0.0, 0.0, 0.0);
+		//glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0.0, 10.0, 0.0, 10.0, -1.0, 1.0);
+
+		Quad();
+		
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(GL_TRUE);
+	return 0;
 }
